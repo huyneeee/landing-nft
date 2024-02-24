e7347b4cc65aad15590c22aa0cb0031d85433b95
khanhduy <sanmario.it@gmail.com>
2024-02-25 00:21:59 +0700

    feat: responsive bottom main hero

--- components/pages/HomePage/BottomMainHero.tsx
+++ components/pages/HomePage/BottomMainHero.tsx
@@ -2,33 +2,42 @@ import TextCircle from "@/components/molecules/TextCircle";
 import Image from "next/image";
 import React from "react";
 
+const ImageGroup = ({ width = 400, height = 530 }: { width?: number; height?: number }) => {
+  return (
+    <div className="list-image flex gap-4 min-sm:-translate-x-[25%] min-sm:gap-[40px]">
+      <Image
+        width={width}
+        height={height}
+        alt="img"
+        src="/images/bottom-hero-1.png"
+        className="overflow-hidden rounded-[30px]"
+      />
+      <Image
+        width={width}
+        height={height}
+        alt="img"
+        src="/images/bottom-hero-2.png"
+        className="overflow-hidden rounded-[30px]"
+      />
+      <Image
+        width={width}
+        height={height}
+        alt="img"
+        src="/images/bottom-hero-3.png"
+        className="overflow-hidden rounded-[30px]"
+      />
+    </div>
+  );
+};
+
 const BottomMainHero = () => {
   return (
     <section className="bottom-main-hero mt-[-260px] overflow-hidden bg-transparent pb-[92px]">
-      <div className="nft-container flex items-center justify-between">
-        <div className="list-image flex -translate-x-[25%] gap-[40px]">
-          <Image
-            width={400}
-            height={530}
-            alt="img"
-            src="/images/bottom-hero-1.png"
-            className="overflow-hidden rounded-[30px]"
-          />
-          <Image
-            width={400}
-            height={530}
-            alt="img"
-            src="/images/bottom-hero-2.png"
-            className="overflow-hidden rounded-[30px]"
-          />
-          <Image
-            width={400}
-            height={530}
-            alt="img"
-            src="/images/bottom-hero-3.png"
-            className="overflow-hidden rounded-[30px]"
-          />
-        </div>
+      <div className="flex justify-center min-sm:hidden">
+        <ImageGroup width={113} height={150} />
+      </div>
+      <div className="nft-container flex items-center justify-between max-sm:hidden">
+        <ImageGroup />
         <TextCircle />
       </div>
     </section>
